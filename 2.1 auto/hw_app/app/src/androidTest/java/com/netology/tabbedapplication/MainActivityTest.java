177c753b5c6a6722640aa85c9d27822c2c28171c
Komshukov Maxim <88716968+komshuk@users.noreply.github.com>
2023-10-17 22:28:50 +0300

    Update MainActivityTest.java

--- 2.1 auto/hw_app/app/src/androidTest/java/com/netology/tabbedapplication/MainActivityTest.java
+++ 2.1 auto/hw_app/app/src/androidTest/java/com/netology/tabbedapplication/MainActivityTest.java
@@ -29,72 +29,18 @@ import org.junit.runner.RunWith;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class MainActivityTest {
+public class Tab2ClickTest {
 
     @Rule
     public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
             new ActivityScenarioRule<>(MainActivity.class);
 
     @Test
-    public void mainActivityTest() {
-        ViewInteraction tabView = onView(
-                allOf(withContentDescription("Tab 1"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.tabs),
-                                        0),
-                                0),
-                        isDisplayed()));
-        tabView.perform(click());
+    public void tab2ClickTest() {
+        onView(withContentDescription("Tab 2"))
+                .perform(click());
 
-        ViewInteraction textView = onView(
-                allOf(withId(R.id.section_label), withText("Page: 1"),
-                        withParent(allOf(withId(R.id.constraintLayout),
-                                withParent(withId(R.id.view_pager)))),
-                        isDisplayed()));
-        textView.check(matches(withText("Page: 1")));
-
-        ViewInteraction tabView2 = onView(
-                allOf(withContentDescription("Tab 2"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.tabs),
-                                        0),
-                                1),
-                        isDisplayed()));
-        tabView2.perform(click());
-
-        ViewInteraction textView2 = onView(
-                allOf(withId(R.id.section_label), withText("Page: 2"),
-                        withParent(allOf(withId(R.id.constraintLayout),
-                                withParent(withId(R.id.view_pager)))),
-                        isDisplayed()));
-        textView2.check(matches(withText("Page: 2")));
-
-        ViewInteraction textView3 = onView(
-                allOf(withId(R.id.section_label), withText("Page: 2"),
-                        withParent(allOf(withId(R.id.constraintLayout),
-                                withParent(withId(R.id.view_pager)))),
-                        isDisplayed()));
-        textView3.check(matches(withText("Page: 2")));
-    }
-
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
-
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
+        onView(allOf(withId(R.id.section_label), withText("Page: 2")))
+                .check(matches(isDisplayed()));
     }
 }
